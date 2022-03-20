e725f1b2bcb94c5bfc88f896c1f2a379308234e8
sammychvz <sammy_chvz17@outlook.com>
2022-03-20 16:46:54 -0500

    added the inital files and created the user model

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        userName: {
+            type: String, 
+            required: true,
+            unique: true, 
+            trim: true 
+        }, 
+        email: {
+            type: String, 
+            required: true,
+            unique: true, 
+        }, 
+        thoughts: {
+
+        }, 
+        friends: {
+            
+        }
+    }
+)
